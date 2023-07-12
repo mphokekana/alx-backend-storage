417e1711ac1a16251d038d1bc3767be374eda229
Mpho Kekana <mphokekana736@gmail.com>
2023-07-12 17:17:03 +0000

    fix code

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
--- 3-glam_rock.sql
--- Lists all bands with Glam rock as their main style, ranked by their longevity
+-- Search bands with style Glam rock
+-- Durantion current
 
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
+FROM metal_bands 
+WHERE style LIKE '%Glam rock%';
+
