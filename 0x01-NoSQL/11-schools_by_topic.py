59206bf2fcbb016ab60b97ee5d55244f84f97132
Mpho Kekana <mphokekana@gmail.com>
2023-07-18 08:12:46 -0700

    Task

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,16 @@
-// Lists all documents with name starting by Holberton in the school collection.
-db.school.find({name: /Holberton.*/}).forEach(printjson);
+#!/usr/bin/env python3
+'''Task 11's module.
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of school having a specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
