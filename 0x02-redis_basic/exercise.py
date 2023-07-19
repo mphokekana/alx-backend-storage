0bb3dc2e722079251896d3f17c7d8420d1f1d022
Mpho Kekana <mphokekana736@.com>
2023-07-19 14:23:37 +0000

    Task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,10 +4,10 @@ from typing import Union
 
 class Cache:
     def __init__(self):
-        self._redis = redis.Redis()
+        self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
+        key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
