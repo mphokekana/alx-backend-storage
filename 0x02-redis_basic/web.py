2a7df4c1f20b3a401de68502a1c9d417aa478a35
Mpho Kekana <mphokekana736@.com>
2023-07-19 19:50:37 +0000

    Task1

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
-"""Implement an expiring web cache and tracker
-"""
+""" Redis Module """
+
+from functools import wraps
 import redis
 import requests
-r = redis.Redis()
-count = 0
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
+def count_requests(method: Callable) -> Callable:
+    """ Decorator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
-    """track url request and cache the result"""
-    r.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
-    return resp.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
