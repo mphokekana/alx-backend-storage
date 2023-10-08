15ee83784b58fed5b479008cda6f7680e3f962d2
Mpho Kekana <111887473+mphokekana@users.noreply.github.com>
2023-10-08 21:46:57 +0000

    Complete task 5

--- 0x00-MySQL_Advanced/5-init.sql
+++ 0x00-MySQL_Advanced/5-init.sql
@@ -12,4 +12,3 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users (email, name) VALUES ("bob@dylan.com", "Bob");
 INSERT INTO users (email, name, valid_email) VALUES ("sylvie@dylan.com", "Sylvie", 1);
 INSERT INTO users (email, name, valid_email) VALUES ("jeanne@dylan.com", "Jeanne", 1);
-
